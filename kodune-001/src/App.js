8dd3142846ae770807ec6a296e81da6f5496364b
akadime <107892133+akadime@users.noreply.github.com>
2022-07-04 12:27:00 +0300

    Kodune-001

---
+++ kodune-001/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <button className="nupp">NUPP</button>
+        <div className="sinine-tekst">Tekst siniseks</div>
+        <div>
+        <h1 className="h1-esimene">Esimene kodutöö</h1>  
+        </div>
+        <img className="pilt" src="https://miro.medium.com/max/1400/1*xo1M1DiOigYrXw4vMQ_u3A.png" alt="Example of italic text" />
+      </body>
+    </div>
+  );
+}
+
+export default App;
